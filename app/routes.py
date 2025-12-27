1755e7f280b39010d4e87e062d269c582a61ac0c
Inaba1995 <149956151+Inaba1995@users.noreply.github.com>
2025-12-27 02:56:37 -0800

    2

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, request, url_for, flash, abort
 
 from app import app
 
-ancet = []
+posts = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,6 +10,6 @@ def index():
         title = request.form['title']
         content = request.form['content']
         if title and content:
-            ancet.append({'title': title, 'content': content})
+            posts.append({'title': title, 'content': content})
             return redirect(url_for('index'))
-    return render_template('index.html', ancet=ancet)
+    return render_template('index.html', posts=posts)
